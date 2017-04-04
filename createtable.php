fb116c1efe3b6558196f7c6742ae9f7828fc6e16
nguyen nguyen <nguyenlun92@gmail.com>
2017-04-05 04:48:41 +0700

    ket noi database

---
+++ createtable.php
@@ -0,0 +1,5 @@
+<?php
+$conn = pg_connect("host=ec2-107-22-223-6.compute-1.amazonaws.com port=5432 dbname=d9rnhagjf81lc4 user=sxezjwxpyzxakx password=e9295d8b7b2c109c0cd69fac8afb216ad34235aae7e6e9a52100e16c0622a326");
+
+echo "connect db";
+?>
